f4c2f128a5262d14b8e23beccfa669672e566940
nh602 <Nicholas.Henley@proton.me>
2025-11-25 12:38:59 -0700

    implemented http server for public access functions only

--- main.go
+++ main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/NH-Homelab/portfolio-backend/internal/config"
 	pgdb "github.com/NH-Homelab/portfolio-backend/internal/pg_db"
 	portfoliodao "github.com/NH-Homelab/portfolio-backend/internal/portfolio_dao"
+	publichandler "github.com/NH-Homelab/portfolio-backend/internal/public_handler"
 )
+
+func logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Received request: %s %s%s from %s\n", r.Method, r.Host, r.URL, r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func setContentType(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	backend_config, err := config.Load()
@@ -29,11 +45,14 @@ func main() {
 	defer pgdb.Close()
 
 	dao := portfoliodao.NewPortfolioDao(pgdb)
+	ph := publichandler.NewPublicHandler(dao)
+	mux := http.NewServeMux()
 
-	p, err := dao.GetAllProjects()
+	ph.RegisterHandlers(mux)
+
+	log.Printf("Starting HTTP server on :8080...")
+	err = http.ListenAndServe(":8080", logRequest(setContentType(mux)))
 	if err != nil {
-		log.Fatalf("Failed to get projects: %v", err)
+		log.Fatalf("HTTP server failed: %v", err)
 	}
-
-	log.Printf("%v", p)
 }
